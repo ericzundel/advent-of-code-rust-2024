13d207285d4cfc38acf1f20bde00ea19dfde1f81
Eric Z. Ayers <ericzundel@gmail.com>
2025-03-06 19:34:30 -0500

    Prepping 16 to work with Arthur

---
+++ src/bin/09.rs
@@ -0,0 +1,38 @@
+advent_of_code::solution!(9);
+
+#[derive (Debug, Clone)]
+struct File {
+    id: u64,
+    start: u64,
+    len: u64,
+}
+
+struct Free {
+    start: u64,
+    len: u64,
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(1928));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
