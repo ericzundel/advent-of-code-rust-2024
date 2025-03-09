bb19edeb786b531eebe7cd3f1ae6abe7669bc005
Eric Z. Ayers <ericzundel@gmail.com>
2025-03-08 21:40:14 -0500

    Day 12 part 1 solved

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -6,45 +6,52 @@ struct Position {
     y: usize,
 }
 
+#[derive(Debug, Clone)]
+struct Plot {
+    plant: char,
+    id: Option<u64>,
+}
+
 struct Garden {
-    plots: Vec<Vec<char>>,
+    plots: Vec<Vec<Plot>>,
+    max_id: u64,
 }
 
 impl Garden {
     pub fn new(input: &str) -> Self {
-        let mut plots = Vec::new();
+        let mut plots: Vec<Vec<Plot>> = Vec::new();
         for line in input.lines() {
-            let mut row: Vec<char> = Vec::new();
+            let mut row: Vec<Plot> = Vec::new();
             let line = line.trim();
             for c in line.chars() {
-                row.push(c);
+                row.push(Plot { plant: c, id: None });
             }
             plots.push(row);
         }
-        Garden { plots }
+        Garden { plots, max_id: 0 }
     }
 
-    fn find_adjacent_fence(&self, curr: Position, value: char) -> u64 {
-        if self.plots[curr.y][curr.x] != value {
+    fn find_adjacent_fence(&self, curr: Position, id: u64) -> u64 {
+        if self.plots[curr.y][curr.x].id != Some(id) {
             return 0;
         }
 
         let mut result: u64 = 0;
         let max_x = self.plots[0].len() - 1;
         let max_y = self.plots.len() - 1;
-        if curr.x > 0 && self.plots[curr.y][curr.x - 1] != value {
+        if curr.x > 0 && self.plots[curr.y][curr.x - 1].id != Some(id) {
             // west
             result += 1;
         }
-        if curr.y > 0 && self.plots[curr.y - 1][curr.x] != value {
+        if curr.y > 0 && self.plots[curr.y - 1][curr.x].id != Some(id) {
             // north
             result += 1;
         }
-        if curr.y < max_y && self.plots[curr.y + 1][curr.x] != value {
+        if curr.y < max_y && self.plots[curr.y + 1][curr.x].id != Some(id) {
             // south
             result += 1;
         }
-        if curr.x < max_x && self.plots[curr.y][curr.x + 1] != value {
+        if curr.x < max_x && self.plots[curr.y][curr.x + 1].id != Some(id) {
             // east
             result += 1;
         }
@@ -63,11 +70,11 @@ impl Garden {
         result
     }
 
-    pub fn calc_area(&self, plot_name: char) -> u64 {
+    pub fn calc_area(&self, id: u64) -> u64 {
         let mut result = 0;
         for y in 0..self.plots.len() {
             for x in 0..self.plots[0].len() {
-                if self.plots[y][x] == plot_name {
+                if self.plots[y][x].id == Some(id) {
                     result += 1;
                 }
             }
@@ -75,30 +82,103 @@ impl Garden {
         result
     }
 
-    pub fn calc_perimeter(&self, plot_name: char) -> u64 {
+    pub fn calc_perimeter(&self, id: u64) -> u64 {
         let mut result = 0;
         for y in 0..self.plots.len() {
             for x in 0..self.plots[0].len() {
-                result += self.find_adjacent_fence(Position { x, y }, plot_name);
+                result += self.find_adjacent_fence(Position { x, y }, id);
             }
         }
         result
     }
 
-    pub fn incorrect_calculate_cost(&self) -> u64 {
+    pub fn calculate_cost(&self) -> u64 {
         let mut result: u64 = 0;
-        for plot_name in 'A'..'Z' {
+        for id in 0..self.max_id {
             result += self
-                .calc_area(plot_name)
-                .checked_mul(self.calc_perimeter(plot_name))
+                .calc_area(id)
+                .checked_mul(self.calc_perimeter(id))
                 .unwrap();
         }
         result
     }
+    // Scan the plots. Adjacent plots with the same plant type get the same id
+    pub fn assign_ids(&mut self) {
+        let mut next_id = 0;
+        for y in 0..self.plots.len() {
+            for x in 0..self.plots[0].len() {
+                if self.plots[y][x].id.is_none() {
+                    self.mark_plots(Position { x, y }, next_id);
+                    next_id += 1;
+                }
+            }
+        }
+        self.max_id = next_id;
+    }
+
+    fn mark_plots(&mut self, curr: Position, id: u64) {
+        self.plots[curr.y][curr.x].id = Some(id);
+        for adj in self.adjacent_plots(curr) {
+            self.mark_plots(adj, id);
+        }
+    }
+
+    fn adjacent_plots(&mut self, curr: Position) -> Vec<Position> {
+        let value = self.plots[curr.y][curr.x].plant;
+        let mut result: Vec<Position> = Vec::new();
+        let max_x = self.plots[0].len() - 1;
+        let max_y = self.plots.len() - 1;
+        let plots = &self.plots;
+        if curr.x > 0 {
+            let plot = &plots[curr.y][curr.x - 1];
+            if plot.id.is_none() && plot.plant == value {
+                // west
+                result.push(Position {
+                    x: curr.x - 1,
+                    y: curr.y,
+                });
+            }
+        }
+        if curr.y > 0 {
+            let plot = &plots[curr.y - 1][curr.x];
+            if plot.id.is_none() && plot.plant == value {
+                // north
+                result.push(Position {
+                    x: curr.x,
+                    y: curr.y - 1,
+                });
+            }
+        }
+        if curr.y < max_y {
+            let plot = &plots[curr.y + 1][curr.x];
+            if plot.id.is_none() && plot.plant == value {
+                // south
+                result.push(Position {
+                    x: curr.x,
+                    y: curr.y + 1,
+                });
+            }
+        }
+        if curr.x < max_x {
+            let plot: &Plot = &plots[curr.y][curr.x + 1];
+            if plot.id.is_none() && plot.plant == value {
+                // east
+                result.push(Position {
+                    x: curr.x + 1,
+                    y: curr.y,
+                });
+            }
+        }
+        result
+    }
 }
+
 pub fn part_one(input: &str) -> Option<u64> {
-    let garden = Garden::new(input);
-    Some(garden.incorrect_calculate_cost())
+    let mut garden = Garden::new(input);
+    garden.assign_ids();
+    
+    // Answer from AOC data is 1477762
+    Some(garden.calculate_cost())
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -108,28 +188,32 @@ pub fn part_two(input: &str) -> Option<u64> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn test_example1() {
         let input: &str = "AAAA\n\
           BBCD\n\
           BBCC\n\
           EEEC\n";
-        let garden = Garden::new(input);
-        assert_eq!(garden.calc_area('F'), 0);
-        assert_eq!(garden.calc_perimeter('F'), 0);
-        assert_eq!(garden.calc_area('A'), 4);
-        assert_eq!(garden.calc_perimeter('A'), 10);
-        assert_eq!(garden.calc_area('B'), 4);
-        assert_eq!(garden.calc_perimeter('B'), 8);
-        assert_eq!(garden.calc_area('C'), 4);
-        assert_eq!(garden.calc_perimeter('C'), 10);
-        assert_eq!(garden.calc_area('D'), 1);
-        assert_eq!(garden.calc_perimeter('D'), 4);
-        assert_eq!(garden.calc_area('E'), 3);
-        assert_eq!(garden.calc_perimeter('E'), 8);
-        assert_eq!(garden.incorrect_calculate_cost(), 4*10 + 4*8 + 4*10 + 1*4+ 3*8);
-        assert_eq!(garden.incorrect_calculate_cost(), 140);
+        let mut garden = Garden::new(input);
+        garden.assign_ids();
+        assert_eq!(garden.calc_area(5), 0);
+        assert_eq!(garden.calc_perimeter(5), 0);
+        assert_eq!(garden.calc_area(0), 4);
+        assert_eq!(garden.calc_perimeter(0), 10);
+        assert_eq!(garden.calc_area(1), 4);
+        assert_eq!(garden.calc_perimeter(1), 8);
+        assert_eq!(garden.calc_area(2), 4);
+        assert_eq!(garden.calc_perimeter(2), 10);
+        assert_eq!(garden.calc_area(3), 1);
+        assert_eq!(garden.calc_perimeter(3), 4);
+        assert_eq!(garden.calc_area(4), 3);
+        assert_eq!(garden.calc_perimeter(4), 8);
+        assert_eq!(
+            garden.calculate_cost(),
+            4 * 10 + 4 * 8 + 4 * 10 + 1 * 4 + 3 * 8
+        );
+        assert_eq!(garden.calculate_cost(), 140);
     }
 
     #[test]
@@ -139,17 +223,22 @@ mod tests {
           OOOOO\n\
           OXOXO\n\
           OOOOO\n";
-        let garden = Garden::new(input);
-        assert_eq!(garden.calc_area('F'), 0);
-        assert_eq!(garden.calc_perimeter('F'), 0);
-        assert_eq!(garden.calc_area('X'), 4);
-        assert_eq!(garden.calc_perimeter('X'), 16);
-        assert_eq!(garden.calc_area('O'), 21);
-        assert_eq!(garden.calc_perimeter('O'), 36);
-        assert_eq!(garden.incorrect_calculate_cost(), 772);
+        let mut garden = Garden::new(input);
+        garden.assign_ids();
+
+        assert_eq!(garden.calc_area(6), 0);
+        assert_eq!(garden.calc_perimeter(6), 0);
         
+        assert_eq!(garden.calc_area(0), 21);
+        assert_eq!(garden.calc_perimeter(0), 36);
+
+        for id in 1..5 {
+            assert_eq!(garden.calc_perimeter(id), 4);
+            assert_eq!(garden.calc_area(id), 1);
+        }
+        assert_eq!(garden.calculate_cost(), 772);
     }
-    
+
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
